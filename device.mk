f948f1e75f94234c51bf0d11ef6238856f905c12
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    santoni: Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -403,7 +403,7 @@ PRODUCT_PACKAGES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.santoni
 
 # Vibrator
 PRODUCT_PACKAGES += \
