ac79989d2a5378c1e82c3bf69d1079a331f0c62d
Ritesh Saxena <riteshsax007@gmail.com>
2018-05-29 15:41:59 +0000

    santoni: Rename biometrics hal
    Change-Id: I71a9b4b2876b5384c00056ca3465523fed6d41a7

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom
+    android.hardware.biometrics.fingerprint@2.0-service.xiaomi_santoni
 
 # FM
 PRODUCT_PACKAGES += \
