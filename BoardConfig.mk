8407828ef41b6c572337ea8892c7245a97468e81
Jyotiraditya <jyotiraditya182@gmail.com>
2018-09-06 00:13:19 +0000

    [DNM]: santoni :for AOSPA
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,11 +118,11 @@ BOARD_USES_QCNE := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT := false
     endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
 
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
