CypherOS/device_xiaomi_santoni

santoni: Boardconfig: Change Kernel soure path
Change-Id: I04be945eb6dae3042c236785328c7c9995d42e49
Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := santoni_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+TARGET_KERNEL_SOURCE := kernel/xiaomi/santoni
 
 # Clang for Kernel
 TARGET_KERNEL_CLANG_COMPILE := true

SilverShades02/platform_device_xiaomi_santoni

BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := santoni_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8940
 
 # Clang for Kernel
 TARGET_KERNEL_CLANG_COMPILE := true