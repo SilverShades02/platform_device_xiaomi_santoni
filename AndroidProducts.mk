a625ba229a54e22ff285d610ef64ef2b967007af
Jyotiraditya <jyotiraditya182@gmail.com>
2019-01-06 16:22:38 +0530

    santoni: Let's join the PA party.
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nitrogen_santoni.mk
+    $(LOCAL_DIR)/pa_santoni.mk
--- pa_santoni.mk
+++ pa_santoni.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2019 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
+# Check for target product
+ifeq (pa_santoni,$(TARGET_PRODUCT))
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/nitrogen/products/common.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Advanced platform features
+#TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
+
+include device/qcom/common/common.mk
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/xiaomi/santoni/device.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := pa_santoni
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := nitrogen_santoni
-BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -31,4 +41,12 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
 
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys"
+
+TARGET_VENDOR := Xiaomi
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+endif
