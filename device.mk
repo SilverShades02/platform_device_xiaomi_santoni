735b2c3660a71b2ab417ca3c66d8cbcf37815954
Anay Wadhera <anay1018@gmail.com>
2019-01-05 20:23:43 -0600

    santoni: don't build extra copies of telephony-ext
    * since that is already in vendor/*
    
    Change-Id: I309b910ffa0107d858d7e5750aca1b33c326da79

--- device.mk
+++ device.mk
@@ -375,11 +375,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    telephony-ext \
     ims-ext-common
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
